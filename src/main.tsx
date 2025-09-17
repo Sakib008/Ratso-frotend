6964772e0a436f257de12432ee104b40fd809793
Sakib008 <mdshakib1902@gmail.com>
2025-09-17 13:53:36 +0530

    feat : auth Provider added

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,19 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
-import { Provider } from "react-redux";
-import { store } from "./reduxStore/store";
-import "./index.css";
-import App from "./App.tsx";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './reduxStore/store';
+import AuthProvider from './components/providers/AuthProvider';
+import './index.css';
+import App from './App.tsx';
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Router>
       <Provider store={store}>
-        <App />
+        <AuthProvider>
+          <App />
+        </AuthProvider>
       </Provider>
     </Router>
   </StrictMode>
